7df53f5fc87fd198caf5a3c6b93bd09953a0adc1
Houssam <houssam.naous1@gmail.com>
2021-02-07 18:31:10 +0100

    added button to increment

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var number: Int = 0
+    @IBAction func perform(_ sender: Any) {
+        print("I'm Pressed")
+        self.number += 1
+        self.label.text = String(number)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
